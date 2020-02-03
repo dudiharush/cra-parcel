d6daf696f8fa588fec094b1df7bc2b51d0f037cd
David Harush <davidharush@gmail.com>
2020-02-03 12:25:24 +0200

    initial commit

---
+++ src/cli.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+const commander = require("commander");
+const chalk = require("chalk");
+const execSync = require("child_process").execSync;
+const packageJson = require("../package.json");
+const fs = require("fs-extra");
+const path = require("path");
+const npm = require("npm-author-name");
+
+let projectName;
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments("<project-directory>")
+  .usage(`${chalk.green("<project-directory>")}`)
+  .action(name => {
+    projectName = name;
+  })
+  .parse(process.argv);
+
+if (typeof projectName === "undefined") {
+  console.error("Please specify the project directory:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("<project-directory>")}`
+  );
+  console.log();
+  console.log("For example:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("my-react-app")}`
+  );
+  process.exit(1);
+}
+
+const projectDestination = path.join(process.cwd(), projectName);
+
+if (fs.existsSync(projectDestination)) {
+  console.log(`The directory ${chalk.green(projectName)} already exists.`);
+  process.exit(1);
+}
+
+const template_path = path.join(__dirname, "..", "templates", "react-app-template");
+
+
+fs.copySync(
+  template_path,
+  projectName
+);
+
+fs.renameSync(`${projectName}/package-tmp.json`, `${projectName}/package.json`)
+
+const packageJsonFileBuffer = fs.readFileSync(`${projectName}/package.json`);
+let packageJsonFileJson = JSON.parse(packageJsonFileBuffer);
+packageJsonFileJson.name = projectName;
+packageJsonFileJson.author = npm.getAuthorName();
+fs.writeFileSync(`${projectName}/package.json`, JSON.stringify(packageJsonFileJson));
+
+function shouldUseYarn() {
+  try {
+    execSync("yarn --version", { stdio: "ignore" });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+process.chdir(projectDestination);
+
+fs.writeFileSync(
+  ".gitignore",
+  `node_modules
+  lib`
+);
+
+if (shouldUseYarn()) {
+  execSync("yarn install", { stdio: [0, 1, 2] });
+} else {
+  execSync("npm install", { stdio: [0, 1, 2] });
+}
---
+++ templates/react-app-template/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h2>Hello React Parcel App</h2>
+    </div>
+  );
+}
